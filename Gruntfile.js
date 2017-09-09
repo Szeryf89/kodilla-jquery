c25a1ba1cf187a3b5cc676975f96cced7cea7e39
marcni <marcin.radomski89@gmail.com>
2017-09-09 13:53:12 +0200

    add module 10.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,50 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        jshint: {
-        all: ['js/*.js']
-        }
-    });
+        // Project configuration.
+        grunt.initConfig({
+            sass: {
+                options: {
+                    sourceMap: true,
+                    outputStyle: 'compressed',
+                    sourceComments: true
+                },
+                dist: {
+                    files: {
+                        'css/style.css': 'scss/style.scss'
+                    }
+                }
+            },
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+            imagemin: {
+                dynamic: {
+                    files: [{
+                        expand: true,
+                        cwd: 'images/',
+                        src: ['**/*.{png,jpg,gif}'],
+                        dest: 'images/build/'
+                    }]
+                }
+            },
 
-    grunt.registerTask('default', ['jshint']);
+            watch: {
+                scripts: {
+                    files: ['scss/*.scss'],
+                    tasks: ['sass'],
+                    options: {
+                        spawn: false,
+                    },
+                }
+            }
+        });
 
-    };
+        // Load the plugins tasks
+        grunt.loadNpmTasks('grunt-sass');
+        grunt.loadNpmTasks('grunt-contrib-imagemin');
+        grunt.loadNpmTasks('grunt-contrib-watch');
+
+        // Default task(s).
+
+        grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+        grunt.registerTask('img', ['imagemin']);
+        grunt.registerTask('scss', ['sass', 'watch']);
+        };
